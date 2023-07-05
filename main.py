b87140ff5ea67e38f6693981d0c2c9ca6dbbb5b3
juanberasategui <jberasategui1@gmail.com>
2023-07-05 16:48:18 +0200

    .

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-from transformers import CLIPProcessor, CLIPModel
-from PIL import Image
-import torch
 import torchvision.transforms as transforms
+import time
+from streamlit_js_eval import streamlit_js_eval
+
+
+
+if "player1" not in st.session_state:
+    st.session_state["player1"] = False
+
+if "player2" not in st.session_state:
+    st.session_state["player2"] = False
+
+if "newplayer" not in st.session_state:
+    st.session_state["newplayer"] = False
+
+
 
 st.title("Fremtind Bordtennis Ranking")
 
@@ -12,4 +23,98 @@ st.write("Her kan du se rankingen til Fremtind Bordtennis")
 
 ranking = pd.read_csv("bordtennis.csv")
 
-st.write(ranking, format="csv")
+#order rows by elo
+ranking = ranking.sort_values(by="Elo", ascending=False)
+
+st.write(ranking)
+
+
+
+if st.button("Er du ikke på ranking? Klikk her for å legge deg til"):
+    st.session_state["newplayer"] = True
+
+if st.session_state["newplayer"]:
+    name_new_player = st.text_input("Skriv inn navnet ditt")
+    if st.button("Legg til " + name_new_player):
+        ranking.loc[len(ranking)] = [name_new_player, 0]
+        
+        ranking.to_csv("bordtennis.csv", index=False)
+        streamlit_js_eval(js_expressions="parent.window.location.reload()")
+
+    
+
+st.write("Skriv inn navn på spillerne som har spilt mot hverandre, og trykk på knappen til den som vant")
+
+name_p1 = st.text_input("Navn på spiller 1")
+name_p2 = st.text_input("Navn på spiller 2")
+
+if not name_p1 and name_p2:
+    st.write("Skriv inn navn på begge spillerne")
+
+if name_p1 and  name_p2:
+#get elo from "Juan"
+    st.write("du valgt " + name_p1 + " og " + name_p2)
+    try:
+        elo_p1 = ranking.loc[ranking["Player"] == name_p1, "Elo"].iloc[0]
+        elo_p2 = ranking.loc[ranking["Player"] == name_p2, "Elo"].iloc[0]
+    except:
+        st.write("En av spillerne er ikke på rankingen")
+        
+        st.write("**Siden oppdateres om 3 sek**")
+        time.sleep(3)
+        streamlit_js_eval(js_expressions="parent.window.location.reload()")
+
+
+    if elo_p1 >= elo_p2:
+        elo_multiplier = elo_p1 / elo_p2
+        if elo_multiplier > 1.5:
+            elo_multiplier = 2
+        else:
+            elo_multiplier = elo_multiplier
+    else:
+        elo_multiplier = elo_p2 / elo_p1
+        if elo_multiplier > 1.5:
+            elo_multiplier = 2
+        else:
+            elo_multiplier = elo_multiplier
+
+
+    st.write("Spiller 1 Elo: " + str(elo_p1))
+    st.write("Spiller 2 Elo : " + str(elo_p2))
+
+    elo_winner = 50 *1/elo_multiplier
+    elo_loser = -50 *1/elo_multiplier
+
+    if st.button(name_p1 + " vant"):
+        st.session_state["player1"] = True
+
+    if st.button(name_p2 + " vant"):
+        st.session_state["player2"] = True
+
+
+    if st.session_state["player1"]:
+        st.write(name_p1 + " vant")
+        ranking.loc[ranking["Player"] == name_p1, "Elo"] += elo_winner
+        ranking.loc[ranking["Player"] == name_p2, "Elo"] += elo_loser
+        ranking.to_csv("bordtennis.csv", index=False)
+        st.write(ranking)
+        
+        st.write("Reload siden for å se oppdatert ranking, siden oppdateres automatisk om 5 sekunder")
+        time.sleep(5)
+        streamlit_js_eval(js_expressions="parent.window.location.reload()")
+        st.session_state["player2"] = False
+        
+
+
+    if st.session_state["player2"]:
+        st.write(name_p2 + " vant")
+        ranking.loc[ranking["Player"] == name_p2, "Elo"] += elo_winner
+        ranking.loc[ranking["Player"] == name_p1, "Elo"] += elo_loser
+        ranking.to_csv("bordtennis.csv", index=False)
+        st.write(ranking)
+        
+        st.write("Reload siden for å se oppdatert ranking, siden oppdateres automatisk om 5 sekunder")
+        time.sleep(5)
+        streamlit_js_eval(js_expressions="parent.window.location.reload()")
+        st.session_state["player2"] = False
+
